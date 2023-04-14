d3f2e011cee61099b20082620d9a4c48e5ceefcd
Jordan Shefner <yarden11111@gmail.com>
2023-04-14 20:53:40 +0300

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+subs = Sub.create([{ creator_word: "פרש", joiner_word: "מלך", difficulty: 3 }, 
+                   { creator_word: "מעיל", joiner_word: "שלג", difficulty: 3 }, 
+                   { creator_word: "כובע", joiner_word: "ים", difficulty: 2 }, 
+                   { creator_word: "מלח", joiner_word: "פלפל", difficulty: 1 },
+                   { creator_word: "יהלום", joiner_word: "תלתן", difficulty: 2 },
+                   { creator_word: "תות", joiner_word: "בננה", difficulty: 1 },
+                   { creator_word: "מנורה", joiner_word: "שמן", difficulty: 2 },
+                   { creator_word: "אוזניות", joiner_word: "משקפיים", difficulty: 1 },
+                   { creator_word: "דבורה", joiner_word: "פרח", difficulty: 1 },
+                   { creator_word: "נס", joiner_word: "קפה", difficulty: 1 }])
