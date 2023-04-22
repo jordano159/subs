4edf32bd66908019f834135f3f9dbb37b0a06f0d
gnpashi <guynatan@gmail.com>
2023-04-22 19:04:37 +0300

    seed

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,4 +1,16 @@
 class Sub < ApplicationRecord
   belongs_to :game, optional: true
   enum :difficulty, [:easy, :medium, :hard]
+
+  def self.easy_subs
+    self.where(difficulty: 0) 
+  end
+
+  def self.medium_subs
+    self.where(difficulty: 1) 
+  end
+
+  def self.hard_subs
+    self.where(difficulty: 2) 
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,13 +7,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-subs = Sub.create([{ creator_word: "פרש", joiner_word: "מלך", difficulty: :hard }, 
-                   { creator_word: "מעיל", joiner_word: "שלג", difficulty: :hard }, 
-                   { creator_word: "כובע", joiner_word: "ים", difficulty: :medium }, 
-                   { creator_word: "מלח", joiner_word: "פלפל", difficulty: :easy },
-                   { creator_word: "יהלום", joiner_word: "תלתן", difficulty: :medium },
-                   { creator_word: "תות", joiner_word: "בננה", difficulty: :easy },
-                   { creator_word: "מנורה", joiner_word: "שמן", difficulty: :medium },
-                   { creator_word: "אוזניות", joiner_word: "משקפיים", difficulty: :easy },
-                   { creator_word: "דבורה", joiner_word: "פרח", difficulty: :easy },
-                   { creator_word: "נס", joiner_word: "קפה", difficulty: :easy }])
+# subs = Sub.create([{ creator_word: "פרש", joiner_word: "מלך", difficulty: :hard }, 
+#                    { creator_word: "מעיל", joiner_word: "שלג", difficulty: :hard }, 
+#                    { creator_word: "כובע", joiner_word: "ים", difficulty: :medium }, 
+#                    { creator_word: "מלח", joiner_word: "פלפל", difficulty: :easy },
+#                    { creator_word: "יהלום", joiner_word: "תלתן", difficulty: :medium },
+#                    { creator_word: "תות", joiner_word: "בננה", difficulty: :easy },
+#                    { creator_word: "מנורה", joiner_word: "שמן", difficulty: :medium },
+#                    { creator_word: "אוזניות", joiner_word: "משקפיים", difficulty: :easy },
+#                    { creator_word: "דבורה", joiner_word: "פרח", difficulty: :easy },
+#                    { creator_word: "נס", joiner_word: "קפה", difficulty: :easy }])
+
+
+CSV.foreach("subs1.csv") do |row|
+    Sub.create(creator_word: row[0], joiner_word: row[1], difficulty: row[2].to_i - 1)
+end
